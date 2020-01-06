18f38a254e97d81f72541041a11cdbe154252789
Tyler <yiqizhang@gmail.com>
2020-01-06 20:06:09 +0000

    head

--- head.js
+++ head.js
@@ -10,3 +10,10 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+const head = function(data) {
+  console.log(data[0]);
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
