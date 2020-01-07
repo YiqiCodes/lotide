6170316b08c97ff18583898cdbe7f58a4c59b3cc
Tyler <yiqizhang@gmail.com>
2020-01-07 22:41:03 +0000

    equalArrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const eqArrays = function(first, second) {
+  if (first === second) return true;
+  if (first == null || second == null) return false;
+  if (first.length != second.length) return false;
+
+  for (let i = 0; i < first.length; ++i) {
+    if (first[i] !== second[i]) return false;
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  const actualResult = eqArrays(actual);
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
