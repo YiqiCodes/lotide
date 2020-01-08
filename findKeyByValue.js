1404e8051baf74c56b623260238b6a875793457f
Tyler <yiqizhang@gmail.com>
2020-01-08 21:54:24 +0000

    findkey done

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let keys = Object.keys(obj); // keys = [ 'sci_fi', 'comedy', 'drama' ]
+
+  let answer;
+  for (const key of keys) {
+    if (value === obj[key]) {
+      answer = key;
+    }
+  }
+  return answer;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
