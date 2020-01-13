eda3ad9af801721012c16f32b039b5ad1ac2165d
Tyler <yiqizhang@gmail.com>
2020-01-13 19:14:06 +0000

    mocha and chai added for head test

--- head.js
+++ head.js
@@ -1,11 +1,7 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = require("./assertEqual");
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 const head = function(data) {
-  console.log(data[0]);
+  return data[0];
 };
 
 module.exports = head;
